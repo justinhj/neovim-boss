2f14986a4a1af1fad9a7cce63ddb7a2910a15bd7
justinhj <justinhj@gmail.com>
2026-09-07 15:09:15 -0700

    Import the msgpack library

--- build.zig
+++ build.zig
@@ -28,17 +28,17 @@ pub fn build(b: *std.Build) void {
     // to our consumers. We must give it a name because a Zig package can expose
     // multiple modules and consumers will need to be able to specify which
     // module they want to access.
-    const mod = b.addModule("neovim_boss", .{
-        // The root source file is the "entry point" of this module. Users of
-        // this module will only be able to access public declarations contained
-        // in this file, which means that if you have declarations that you
-        // intend to expose to consumers that were defined in other files part
-        // of this module, you will have to make sure to re-export them from
-        // the root file.
-        .root_source_file = b.path("src/root.zig"),
-        // Later on we'll use this module as the root module of a test executable
-        // which requires us to specify a target.
+    const msgpack = b.dependency("zig_msgpack", .{
         .target = target,
+        .optimize = optimize,
+    }).module("zig_msgpack");
+
+    const mod = b.addModule("neovim_boss", .{
+        .root_source_file = b.path("src/root.zig"),
+        .target = target,
+        .imports = &.{
+            .{ .name = "zig_msgpack", .module = msgpack },
+        },
     });
 
     // Here we define an executable. An executable needs to have a root module
@@ -73,12 +73,8 @@ pub fn build(b: *std.Build) void {
             // List of modules available for import in source files part of the
             // root module.
             .imports = &.{
-                // Here "neovim_boss" is the name you will use in your source code to
-                // import this module (e.g. `@import("neovim_boss")`). The name is
-                // repeated because you are allowed to rename your imports, which
-                // can be extremely useful in case of collisions (which can happen
-                // importing modules from different packages).
                 .{ .name = "neovim_boss", .module = mod },
+                .{ .name = "zig_msgpack", .module = msgpack },
             },
         }),
     });
